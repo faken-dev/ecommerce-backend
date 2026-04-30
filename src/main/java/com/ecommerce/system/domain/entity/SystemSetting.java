6d9fc8adf5c6a1f58030b4fdfa2ef1f8a1439af3
faken <faken.dev@gmail.com>
2026-04-30 15:18:22 +0700

    feat(system-setting): add SystemSetting entity for configuration management

---
+++ src/main/java/com/ecommerce/system/domain/entity/SystemSetting.java
@@ -0,0 +1,17 @@
+package com.ecommerce.system.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SystemSetting {
+    private String key;
+    private String value;
+    private String group;
+    private String description;
+}
